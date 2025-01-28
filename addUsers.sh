bee3f5da96267fb334020d5dd3e2f791b27c9e03
MSQZA <jaynna10@yahoo.com>
2025-01-28 12:56:39 +0100

    addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,6 +1,8 @@
 # Script de création d’utilisateurs en Bash
 ## Nom : addUsers.sh
 ## 3utilisateur : user1, user2, user3
+![Capture d'écran 2025-01-28 114059](https://github.com/user-attachments/assets/57aa1166-a245-44d0-b9b3-fe76cbf0cfba)
 
 ## Utilisateur créé
+![Capture d'écran 2025-01-28 114107](https://github.com/user-attachments/assets/231c89a9-f1a8-4a06-9c00-fdc6c4379ebd)
 
